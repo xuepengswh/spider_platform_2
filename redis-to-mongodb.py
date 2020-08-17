8afd7b06b330c01e2f0c3c23f5a5c1a5f747b82e
xuepengswh <l826898728@163.com>
2020-08-17 09:19:29 +0800

    添加redis写数据到mongodb文件

---
+++ redis-to-mongodb.py
@@ -0,0 +1,45 @@
+import json
+import time
+import redis
+import pymongo
+import configparser
+
+def connect_mongo():
+    mongourl = "mongodb://" + mongoUser + ":" + mongoPassword + "@" + mongoHost + ":" + mongoPort
+    conn = pymongo.MongoClient(mongourl)
+    db = conn.mongoDatabase#数据库名
+    return db
+
+def connect_db():
+    myredis =redis.Redis(host=redisHost, port=redisPort, decode_responses=True, password=redisPassword, db=redisDb)
+    data_key_name = redis_platform_address+":temporary"
+    mymongo_db = connect_mongo()
+    while True:
+        dataNum = myredis.llen(data_key_name)
+        if dataNum>1000:
+            for i in range(1000):
+                oneData = myredis.lpop(data_key_name)
+                oneData = json.loads(oneData)
+                mymongo_db["policy_content"].insert_one(oneData)
+        else:
+            time.sleep(10)
+
+if __name__=="__main__":
+    # 读取配置文件
+    configPath = "config.ini"
+    WebConfig = configparser.ConfigParser()
+    WebConfig.read(configPath, encoding='utf-8-sig')
+    redisHost = WebConfig.get("redis", "host")
+    redisPort = WebConfig.get("redis", "port")
+    redisPassword = WebConfig.get("redis", "password")
+    redisDb = WebConfig.get("redis", "database")
+    redis_platform_address = WebConfig.get("redis", "redis_platform_address")
+
+    mongoHost = WebConfig.get("mongodb", "host")
+    mongoPort = WebConfig.get("mongodb", "port")
+    mongoUser = WebConfig.get("mongodb", "user")
+    mongoPassword = WebConfig.get("mongodb", "password")
+    mongoDb = WebConfig.get("mongodb", "database")
+
+
+    connect_db()
