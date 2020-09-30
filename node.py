053c8fdfa5764047ee02df705acf92be145b1734
xuepengswh <l826898728@163.com>
2020-09-30 11:23:05 +0800

    更改headers错误，由header更改为headers

--- node.py
+++ node.py
@@ -146,7 +146,7 @@ class Main():
 
         # 更新模板设置状态
         if "headers" in tempData:
-            self.headers = tempData["header"]
+            self.headers = tempData["headers"]
         else:
             self.headers = {  "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"  }
         if "timeout" in tempData:
